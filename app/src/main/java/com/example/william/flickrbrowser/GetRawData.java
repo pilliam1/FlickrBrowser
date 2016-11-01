4cec3f9e4b78230d61025976c7f043bc8724a2df
William Macleod <willrmacleod@gmail.com>
2016-11-01 11:01:24 +0800

    commit 6

--- app/src/main/java/com/example/william/flickrbrowser/GetRawData.java
+++ app/src/main/java/com/example/william/flickrbrowser/GetRawData.java
@@ -20,7 +20,7 @@ enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK}
 //providing a string url and getting a string back
 //create a enum to hold a list of download statuses
 //enum is containing all the states/possibilities this class can be in
-//initial commit x5
+//initial commit x6
 
 class GetRawData extends AsyncTask<String, Void, String> {
     private static final String TAG = "GetRawData";
